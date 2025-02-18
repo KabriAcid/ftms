fc3960ec76e97d8c5d8b202d322d9391feca8ee3
Regular001(1053) <kabriacid01@gmail.com>
2025-02-18 13:57:28 +0100

    commit

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Family Tree Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <h1>AA CHEKO</h1>
+    <p class="text-center"><a href="resources/views/register.php">Go to register</a></p>
 </body>
+
 </html>
